49c73ff8cdce93efdf78e85eee471c8662b5b281
Michael Corcias <michael.corcias@gelt.com>
2025-04-21 17:40:31 +0200

    avances anteriores

---
+++ data/users.js
@@ -0,0 +1,16 @@
+
+
+module.exports = [
+    {
+        id: 1,
+        username: "user1",
+        password: "password1",
+        name: "User Uno"
+    },
+    {
+        id: 2,
+        username: "user2",
+        password: "password2",
+        name: "User Dos" 
+    }
+]
---
+++ routes/users.js
@@ -0,0 +1,43 @@
+const express = require("express")
+const jwt = require("jsonwebtoken")
+const bcrypt = require("bcryptjs")
+const user = require("../data/users")
+const { secret } = require("../crypto/config")
+const router = express.Router()
+
+
+
+router.post("/login", (req, res) => {
+    const { username, password } = req.body
+    const user = users.find(u => u.username === username)
+
+    if(!user) {
+        return res.status(401).json({ message: "Contraseñas Incorrecta" })
+    }
+
+    const token = jwt.sign({ id: user.id, username: user.username }, secret, {expiresIn: "1h" })
+
+    res.json({ message: "Contraseña Correcta", token })
+})
+
+router.get("/dashboard", (req, res) => {
+    const token = req.header.authorization?.split("")[1]
+
+    if(!token) {
+        return res.status(401).json({ message: "no hya token" })
+    }
+
+    try {
+        const decoded = jwt.verify(token, secret)
+        res.json({ message: `Accesos correctos, ${decoded.username}`})
+    } catch (error) {
+        res.status(401).json({ message: "Token no válido o expirado" })
+    }
+})
+
+router.post("/logout", (req, res) => {
+    req.session.destroy()
+    res.json({ message: "Log out" })
+})
+
+module.exports = router
